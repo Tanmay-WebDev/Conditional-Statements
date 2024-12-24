2888e79de7d9f7ae772f41d0589635821c327b44
Tanmay-WebDev <umas81261@gmail.com>
2024-12-24 19:00:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var a = 4
+var b = 3
+var c = 6
+
+{
+    let a = 10 
+
+    var b = 76
+
+    const c = 30
+}
+
+document.write("This is the value of a:  "+ a)
+document.write("<br>")
+document.write("This is the value of b:  "+ b)
+document.write("<br>")
+document.write("This is the value of c:  "+ c)
